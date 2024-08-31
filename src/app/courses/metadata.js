3ebfadb4505c8c8cf9cfc39e2340bc00c17d150a
GabrielHTissi <gabriel.hoeft.tissi@icloud.com>
2024-08-31 09:40:54 -0300

    Só modifiquei aquele quadrado de ontem

--- src/app/courses/metadata.js
+++ src/app/courses/metadata.js
@@ -10,5 +10,17 @@ export const courses = [
                 Principios C e etc
             </div>
         </div>
+    }, 
+    {
+        title: "Fundamentos C",
+        id: "fundamentos_c_01",
+        progress: 0,
+        content: <div>
+            <h1>Fundamentos C</h1>
+            <hr/>
+            <div>
+                Principios C e etc
+            </div>
+        </div>
     }
 ];
