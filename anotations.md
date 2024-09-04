03a78c4785b0d95765e67080586c99e2163647f4
Priscila <priscilatorres2023@gmail.com>
2024-09-04 13:45:28 -0300

    feat: inicio da lógica para buscar cursos em andamento

--- anotations.md
+++ anotations.md
@@ -5,3 +5,8 @@ https://www.youtube.com/watch?v=wm5gMKuwSYk
 https://mongoosejs.com/docs/queries.html
 
 https://nextjs.org/docs
+
+A fazer:
+
+Precisa pensar em como vai funcionar a lógica de exercícios
+Precisa conferir se vale a pena separar cursos em "steps"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,45 @@
+"use client";
+
 import { Carousel } from "@/components/Carousel";
 import { Section } from "@/components/Section";
+import { getApiURL } from "@/lib/helper";
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { LoadingSection } from "@/components/LoadingSection";
 
 export default function Home() {
+  const { data: session } = useSession();
+  const [loading, setLoading] = useState(true);
+  const [coursesInProgress, setCoursesInProgress] = useState([]);
+
+  useEffect(() => {
+    const fetchProgress = async () => {
+      const data = await fetch(`/api/progressCourse/${session?.user?.id}`);
+      const savedProgress = await data.json();
+
+      console.log(savedProgress);
+      if (Array.isArray(savedProgress)) {
+        setCoursesInProgress(savedProgress);
+      } else setCoursesInProgress([]);
+      setLoading(false);
+    };
+
+    if (session?.user?.id) fetchProgress();
+  }, [session?.user?.id]);
+
   return (
     <div>
-      <Section>
-        aqui ficam os cursos
-        
-      </Section>
+      {
+        loading ?
+          <LoadingSection />
+          :
+          <Section>
+            <div>
+              cursos em andamento
+            </div>
+          </Section>
+      }
     </div>
   );
 }
