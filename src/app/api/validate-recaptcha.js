9668a3f13e391be7f30cecf1d113009578522576
Priscila <priscilatorres2023@gmail.com>
2024-10-18 21:42:51 -0300

    initializing

---
+++ src/app/api/validate-recaptcha.js
@@ -0,0 +1,33 @@
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { token } = req.body;
+
+    try {
+      const verificationUrl = `https://recaptchaenterprise.googleapis.com/v1/projects/${process.env.GCLOUD_CLIENT_ID}/assessments?key=${process.env.RECAPTCHA_SECRET_KEY}`;
+
+      const response = await fetch(verificationUrl, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          event: {
+            token,
+            siteKey: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
+            expectedAction: 'submit'
+          }
+        })
+      });
+
+      const data = await response.json();
+
+      if (data.tokenProperties.valid && data.riskAnalysis.score > 0.5) {
+        return res.status(200).json({ success: true });
+      } else {
+        return res.status(400).json({ success: false, error: 'Invalid token' });
+      }
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error.message });
+    }
+  } else {
+    res.status(405).end();
+  }
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,39 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { authMiddleware } from "next-firebase-auth-edge";
 import { clientConfig, serverConfig } from "./config";
 
+async function validateRecaptcha(token: string): Promise<boolean> {
+  const verificationUrl = `https://recaptchaenterprise.googleapis.com/v1/projects/${process.env.GCP_PROJECT_ID}/assessments?key=${process.env.RECAPTCHA_SECRET_KEY}`;
+  
+  const response = await fetch(verificationUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      event: {
+        token,
+        siteKey: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
+        expectedAction: 'submit',
+      }
+    })
+  });
+
+  const data = await response.json();
+  return data.tokenProperties.valid && data.riskAnalysis.score > 0.5;
+}
+
 export async function middleware(request: NextRequest) {
+  const recaptchaToken = request.headers.get('recaptcha-token');
+
+  if (!recaptchaToken) {
+    return NextResponse.json({ message: 'reCAPTCHA token is missing' }, { status: 400 });
+  }
+
+  const isRecaptchaValid = await validateRecaptcha(recaptchaToken);
+
+  if (!isRecaptchaValid) {
+    return NextResponse.json({ message: 'Invalid reCAPTCHA' }, { status: 400 });
+  }
+
   return authMiddleware(request, {
     loginPath: "/api/login",
     logoutPath: "/api/logout",
