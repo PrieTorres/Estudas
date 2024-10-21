a06693a2ddb812b9b9dfa5a5e1f1ad7fdd59cc93
Priscila <priscilatorres2023@gmail.com>
2024-10-21 15:03:28 -0300

    fix: some minor fixes

---
+++ src/app/api/helper.ts
@@ -0,0 +1,22 @@
+interface ResponseReq {
+  message: string;
+  status: number;
+}
+
+export function checkAuth(req: Request): ResponseReq | undefined {
+  const TOKEN = process.env.AUTH_TOKEN;
+
+  if (!TOKEN) {
+    return { message: "Server configuration error: AUTH_TOKEN is not set", status: 500 };
+  }
+
+  const authHeader = req.headers.get("Authorization");
+  const token = authHeader?.split(" ")[1];
+
+  console.log("token", token);
+  console.log("TOKEN", TOKEN);
+
+  if (token !== TOKEN) {
+    return { message: "Unauthorized",  status: 401 };
+  }
+}
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,16 +7,16 @@ export const Footer = (): ReactElement => {
   return (
     <Container>
       <div style={{ display: 'flex', flexDirection: "column", alignItems: "center", gap: 4 }}>
-        <TranslatedSpan>contact_me</TranslatedSpan>
+        {/*<TranslatedSpan>contact_me</TranslatedSpan>
         <div>
           <div style={{ display: "flex" }}><TranslatedSpan>email</TranslatedSpan>: priscilatorresbdepaula@gmail.com</div>
           <div style={{ display: "flex" }}><TranslatedSpan>cellphone</TranslatedSpan>: +55 (47) 99227-1283</div>
-        </div>
+        </div>*/}
       </div>
       <div style={{ display: "flex", gap: 8 }}>
         <div>&copy; </div>
         <TranslatedSpan>all_rights_reserved</TranslatedSpan>
-        <div> Priscila Torres 2024</div>
+        <div> Católica, Jaraguá do Sul 2024</div>
       </div>
     </Container>
   )
