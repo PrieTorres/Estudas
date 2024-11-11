7e0a03ae06cfd8a94ea590ad47a04c82ddabf02e
Priscila <priscilatorresbdepaula@gmail.com>
2024-11-11 20:12:36 -0300

    some code adjusts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,16 @@
 "use client"
-import { createContext, ReactElement, ReactNode, useContext, useEffect, useState } from 'react';
+import {  ReactElement } from 'react';
 import { Container } from './styles';
 import { Menu } from '../Menu';
 import { SignButtons } from '../SingButtons';
 import { MainLogo } from '../MainLogo';
-import { json } from 'stream/consumers';
-import { ThemeProvider } from 'styled-components';
 import { useThemeContext } from '../Provider/Provider';
 
 
 
 export const Header = (): ReactElement => {
   const { isLight, changeTheme } = useThemeContext();
-  // const [isLight,setIslight]= useState<boolean>(false);
-  // useEffect(() =>{
-  //   console.log(isLight);    
-  // },[isLight]);
-  // let changeTheme=() => {
-  //   setIslight(Boolean(localStorage.getItem("IsLight")));
-  //   console.log(isLight);
-  //   setIslight(!Boolean(isLight));
-  //   localStorage.setItem("IsLight",JSON.stringify(isLight))
-  // }
+ 
   return (
       <Container className='flex '>
         <MainLogo />
--- src/components/Provider/Provider.tsx
+++ src/components/Provider/Provider.tsx
@@ -2,14 +2,13 @@
 import { createContext, ReactElement, ReactNode, useContext, useEffect, useState } from 'react';
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "styled-components";
-import { theme,themeLight } from "@/Styles/theme";
+import { theme, themeLight } from "@/Styles/theme";
 import { GlobalStyles } from "@/Styles/global-styles";
 import StyledComponentsRegistry from '@/lib/registry';
 import useI18n from '@/hooks/useI18n';
 import { PageProvider } from '@/context/pageContext';
 import { DefaultTheme } from 'styled-components/dist/types';
 
-//
 interface ThemeContextType {
   isLight: boolean;
   changeTheme: () => void;
@@ -24,12 +23,11 @@ export const useThemeContext = () => {
   }
   return context;
 };
-//
 
 export const Provider = ({ children, session }: { children: ReactNode, session?: any; }): ReactElement => {
   useI18n();
-  const [mounted,setMounted] =useState(false);
-  const [currnetTheme,setCurrnetTheme] =useState<DefaultTheme>(theme);
+  const [mounted, setMounted] = useState(false);
+  const [currentTheme, setCurrentTheme] = useState<DefaultTheme>(theme);
 
   const [isLight, setIsLight] = useState<boolean>(false);
 
@@ -41,14 +39,13 @@ export const Provider = ({ children, session }: { children: ReactNode, session?:
     }
   }, []);
   useEffect(() => {
-    // Update theme based on isLight state
-    setCurrnetTheme(isLight ? themeLight : theme);
+    setCurrentTheme(isLight ? themeLight : theme);
   }, [isLight]);
 
 
   const changeTheme = () => {
     const newTheme = !isLight;
-    console.log(newTheme);
+
     setIsLight(newTheme);
     localStorage.setItem("IsLight", JSON.stringify(newTheme));
   };
@@ -56,13 +53,13 @@ export const Provider = ({ children, session }: { children: ReactNode, session?:
   return (
     <SessionProvider session={session}>
       <StyledComponentsRegistry>
-      <ThemeContext.Provider value={{ isLight, changeTheme }}>
-        <ThemeProvider theme={currnetTheme}>
-          <GlobalStyles />
-          <PageProvider>
-            {children}
-          </PageProvider>
-        </ThemeProvider>
+        <ThemeContext.Provider value={{ isLight, changeTheme }}>
+          <ThemeProvider theme={currentTheme}>
+            <GlobalStyles />
+            <PageProvider>
+              {children}
+            </PageProvider>
+          </ThemeProvider>
         </ThemeContext.Provider>
       </StyledComponentsRegistry>
     </SessionProvider>
